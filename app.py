eb16541ce92cef9b61d757fd204bc724e5ade808
Laiza <laizanunes.arabian@gmail.com>
2024-11-14 21:32:09 -0300

    app.py

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
-print("oi oi")
-fruta = input("Digite o nome de uma comida: ")
-print(fruta)
+qtd = 0
+soma = 0
+media = 0
+valor = float(input("digite um valor: "))
+
+while (valor > 0.0):
+    soma = soma + valor
+    qtd = qtd + 1
+    valor = float(input("Digite um valor: "))
+
+media = soma / qtd
+print("\ntotal da soma:", soma)
+print("\n qtd digitada: ", qtd)
+print("\n media dos valores: ", media)
